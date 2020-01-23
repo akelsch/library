c094f93d886587ef0d522cb468b754d6aff5072e
Arthur Kelsch <akelsch@users.noreply.github.com>
2020-01-23 19:24:53 +0100

    Import project files

---
+++ src/main/java/de/htwsaar/sar/library/LibraryApplication.java
@@ -0,0 +1,12 @@
+package de.htwsaar.sar.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryApplication.class, args);
+    }
+}
---
+++ src/test/java/de/htwsaar/sar/library/LibraryApplicationTests.java
@@ -0,0 +1,12 @@
+package de.htwsaar.sar.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
